a205aee09344bda9c1cd5b40b91f265a50593c01
brendan-robert-1 <brendan.robert50point@gmail.com>
2018-04-13 12:39:22 -0400

    added dictionary validation to command line word entries

--- src/main/java/com/brobert/CommandLineWordGenerator.java
+++ src/main/java/com/brobert/CommandLineWordGenerator.java
@@ -3,6 +3,12 @@
  */
 package com.brobert;
 
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @author brobert
  *
@@ -43,8 +49,42 @@ public class CommandLineWordGenerator implements StartingWordGenerator {
 			System.exit(1);
 			return null;
 		} else {
+			inDictionary();
 			return commandLineWord;
 		}
+	}
+
+
+
+	/**
+	 *
+	 */
+	private void inDictionary() {
+		List<String> allWord = getAllWords();
+		if (allWord.contains(commandLineWord) == false) {
+			System.out.println(commandLineWord + " is not in the dictionary. Not valid.");
+			System.exit(1);
+		}
+	}
+
+
+
+	/**
+	 * @return
+	 */
+	private List<String> getAllWords() {
+		List<String> allWords = new ArrayList<>();
+
+		try (InputStream is = getClass().getResourceAsStream("/words.txt");
+				BufferedReader read = new BufferedReader(new InputStreamReader(is));) {
+			String line = null;
+			while ((line = read.readLine()) != null) {
+				allWords.add(line);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return allWords;
 	}
 
 }
